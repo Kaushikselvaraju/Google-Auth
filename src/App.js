cd76e33d0da4fef8c9bce474587f6a0d98946bc9
Ckishorepandian <kishorepandian421@gmail.com>
2021-12-21 11:24:33 +0530

    updated ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { RippleLoader } from "load-animations-react";
+import { EllipsisLoader } from "load-animations-react";
 import LoginButton from "./components/LoginButton";
 // import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
@@ -8,7 +8,7 @@ import Profile from "./components/Profile";
 function App() {
   const { isLoading } = useAuth0()
   if (isLoading) return <div className="flex flex-col items-center justify-center h-screen">
-    <RippleLoader />
+    < EllipsisLoader/>
   </div>
 
   return (
